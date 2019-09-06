f660e9a11e2580a33f564f519a9d91bd6e7ddadb
athagi <at.hgt.dev@gmail.com>
2019-09-06 12:12:43 +0900

    ABC 131_c

--- atcoder_beginner/131/131_c/main.go
+++ atcoder_beginner/131/131_c/main.go
@@ -8,44 +8,24 @@ func main() {
 	var a, b, c, d int
 	fmt.Scan(&a, &b, &c, &d)
 
-	cStart := 0
-	cEnd := 0
-	cNum := 0
-	if c > b {
-		cNum = 0
-	} else {
-		cStart = a / c
-		cEnd = b / c
-		cNum = cEnd - cStart
-	}
+	cStart := (a - 1) / c
+	cEnd := b / c
+	cNum := cEnd - cStart
 
-	dStart := 0
-	dEnd := 0
-	dNum := 0
-
-	if d > b {
-
-	} else {
-		dStart = a / d
-		dEnd = b / d
-		dNum = dEnd - dStart
-	}
+	dStart := (a - 1) / d
+	dEnd := b / d
+	dNum := dEnd - dStart
 
 	cdNum := 0
 	if cNum > 0 && dNum > 0 {
-
 		minCommonMultiple := c * d / getMinCommonDivisor(c, d)
-		if minCommonMultiple <= b {
 
-			cdStart := a / minCommonMultiple
-			cdEnd := b / minCommonMultiple
+		cdStart := (a - 1) / minCommonMultiple
+		cdEnd := b / minCommonMultiple
 
-			cdNum = cdEnd - cdStart
-		}
+		cdNum = cdEnd - cdStart
 
 	}
-	// fmt.Println(c * d / getMinCommonDivisor(c, d))
-	// fmt.Println(cNum, dNum, cdNum)
 	res := b - a + 1 - (cNum + dNum - cdNum)
 	fmt.Println(res)
 }
