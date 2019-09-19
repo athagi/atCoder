c23086afc99344083aea9e65aeaa05b31b465e35
athagi <at.hgt.dev@gmail.com>
2019-09-19 16:06:05 +0900

    127_c コインを振らないパターン忘れてた

--- atcoder_beginner/127/127_c/main.go
+++ atcoder_beginner/127/127_c/main.go
@@ -2,43 +2,27 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
-	var N, M int
-	fmt.Scan(&N, &M)
+	var N, K int
+	fmt.Scan(&N, &K)
 
-	inputs := scanNums(M)
-	maxL := 1
-	minR := N
-	for _, v := range inputs {
-		L := v[0]
-		R := v[1]
-		if maxL <= L {
-			maxL = L
+	res := float64(0)
+	for i := 1; i <= N; i++ {
+		tmp := i
+		if i >= K {
+			res += (float64(1) / float64(N))
+			continue
 		}
-		if minR >= R {
-			minR = R
+		for j := 1; ; j++ {
+			tmp = tmp * 2
+			if tmp >= K {
+				res += (float64(1) / float64(N)) * math.Pow(0.5, float64(j))
+				break
+			}
 		}
-	}
-
-	res := minR - maxL + 1
-	if res < 0 {
-		res = 0
 	}
 	fmt.Println(res)
-
-}
-
-func scanNums(len int) (nums [][]int) {
-	var numL, numR int
-
-	for i := 0; i < len; i++ {
-		fmt.Scan(&numL, &numR)
-		LR := make([]int, 0)
-		LR = append(LR, numL)
-		LR = append(LR, numR)
-		nums = append(nums, LR)
-	}
-	return
 }
